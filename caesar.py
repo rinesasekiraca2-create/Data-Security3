2064109968e9d4381853a6fafdfe7dda215f9f7c
rinesasekiraca2-create <rinesa.sekiraca2@student.uni-pr.edu>
2026-05-08 11:24:02 +0200

    update caesar.py
    Implement Caesar cipher character shifting

--- caesar.py
+++ caesar.py
@@ -1,3 +1,13 @@
 def caesar_encrypt(text, key):
     result = ""
     key = int(key)
+
+     for char in text:
+        if char.isalpha():
+            base = 65 if char.isupper() else 97
+            result += chr((ord(char) - base + key) % 26 + base)
+        else:
+            result += char
+
+    return result
+
