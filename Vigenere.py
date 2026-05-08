61f7317bf8eb4feee0b2131f0df21da45561d4b3
djellzaramaja <djellza.ramaja@student.uni-pr.edu>
2026-05-08 23:55:32 +0200

    Encrypting the text and checking every letter

--- Vigenere.py
+++ Vigenere.py
@@ -5,3 +5,23 @@ def vigenere_encrypt(text, key):
     key = key.upper()
 
     key_index = 0
+
+  for char in text:
+
+        if char.isalpha():
+
+            shift = ord(key[key_index % len(key)]) - 65
+
+            base = 65 if char.isupper() else 97
+
+            result += chr((ord(char) - base + shift) % 26 + base)
+
+            key_index += 1
+
+        else:
+
+            result += char
+
+
+
+    return result
