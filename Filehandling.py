fee786aa6e0ac02476d0cda1dbf9ba75570ee238
Elsa-coder11 <elsa.shaqiri@student.uni-pr.edu>
2026-05-09 15:00:04 +0200

    Add file path validation

--- Filehandling.py
+++ Filehandling.py
@@ -15,3 +15,16 @@ def browse_output():
 
     output_path.delete(0, tk.END)
     output_path.insert(0, path)
+
+# Kontrollon validimin e inputeve
+def validate_inputs():
+
+    key = key_entry.get()
+    algorithm = algorithm_var.get()
+
+
+    # Kontrollon file-at
+    if not input_path.get() or not output_path.get():
+
+        messagebox.showerror("Error", "Zgjidh fajllin hyrës dhe dalës!")
+        return False
