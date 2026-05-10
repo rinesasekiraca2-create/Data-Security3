e65643b38933b12fe743d4960f1dd35c640521c3
rinesasekiraca2-create <rinesa.sekiraca2@student.uni-pr.edu>
2026-05-10 10:12:52 +0200

    update gui2.py
    Add key input field to GUI

--- GUI2.py
+++ GUI2.py
@@ -5,3 +5,7 @@ root.geometry("600x550")
 tk.Label(root, text="Zgjedh Algoritmin:").pack()
 algorithm_var = tk.StringVar(value="Caesar")
 tk.OptionMenu(root, algorithm_var, "Caesar", "Vigenere").pack()
+
+tk.Label(root, text="Key:").pack()
+key_entry = tk.Entry(root)
+key_entry.pack()
