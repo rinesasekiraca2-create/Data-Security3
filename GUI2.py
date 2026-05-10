bd66acd58c41d2f32aba7430274b088949770a29
rinesasekiraca2-create <rinesa.sekiraca2@student.uni-pr.edu>
2026-05-10 10:11:55 +0200

    update gui1.py
    Add algorithm selection dropdown to GUI

--- GUI2.py
+++ GUI2.py
@@ -1,3 +1,7 @@
 root = tk.Tk()
 root.title("Encryption / Decryption App")
 root.geometry("600x550")
+
+tk.Label(root, text="Zgjedh Algoritmin:").pack()
+algorithm_var = tk.StringVar(value="Caesar")
+tk.OptionMenu(root, algorithm_var, "Caesar", "Vigenere").pack()
