57ee884c4d5a53f664669376c0378707e36a00f5
rinesasekiraca2-create <rinesa.sekiraca2@student.uni-pr.edu>
2026-05-10 10:13:51 +0200

    update gui.py
    Add browse button for input file

--- GUI2.py
+++ GUI2.py
@@ -1,3 +1,6 @@
+from Filehandling import browse_input
+
+
 root = tk.Tk()
 root.title("Encryption / Decryption App")
 root.geometry("600x550")
@@ -9,3 +12,8 @@ tk.OptionMenu(root, algorithm_var, "Caesar", "Vigenere").pack()
 tk.Label(root, text="Key:").pack()
 key_entry = tk.Entry(root)
 key_entry.pack()
+
+tk.Label(root, text="Input File:").pack()
+input_path = tk.Entry(root)
+input_path.pack()
+tk.Button(root, text="Browse Input", command=browse_input).pack()
